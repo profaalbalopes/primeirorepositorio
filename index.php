28b7351a3f59310d414af09851ca705af73ef7f4
profaalbalopes <78361086+profaalbalopes@users.noreply.github.com>
2021-02-01 10:52:56 -0300

    Inicializar o repositório com o arquivo index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+<html>
+  <head>
+    <title>Primeiro Repositório</title>
+  </head>
+  <body>
+    <h1>
+    <?php
+      echo "Esse é o meu primeiro código em repositório";
+    ?>
+    </h1:>
+  </body>
+</html>
